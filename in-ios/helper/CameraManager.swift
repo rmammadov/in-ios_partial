f872766087fd0b9379ecd565fe91c966d336b26f
Rahman Mammadov <rehman_mv@yahoo.com>
2018-07-23 09:24:28 +0400

    CameraManager skeleton

---
+++ in-ios/helper/CameraManager.swift
@@ -0,0 +1,13 @@
+//
+//  CameraHelper.swift
+//  in-ios
+//
+//  Created by Rahman Mammadov on 7/22/18.
+//  Copyright © 2018 com.innodemneurosciences. All rights reserved.
+//
+
+import Foundation
+
+class CameraManager: NSObject {
+    
+}
