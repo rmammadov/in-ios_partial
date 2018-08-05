f9d8a326cd9f2dd6f6d3d54ea02305da58717359
Rahman Mammadov <rehman_mv@yahoo.com>
2018-08-06 00:43:26 +0400

    Json input_screen_id parsing implemented

--- in-ios/view/model/MenuItem.swift
+++ in-ios/view/model/MenuItem.swift
@@ -11,12 +11,13 @@ import Foundation
 struct MenuItem: Decodable {
     
     let sub_menu_item_item_ids: Array<Int>
-    let translations: [TranslationItem]
     let disable_text_to_speech: Bool
     let id: Int
     let name: String
     let icon: IconItem?
+    let input_screen_id: Int?
     let roles: Array<String>
+    let translations: [TranslationItem]
 }
 
 struct TranslationItem: Decodable {
