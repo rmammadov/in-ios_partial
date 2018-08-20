12a1f36bebb64c83ad298922b22928436d8ad043
Rahman Mammadov <rehman_mv@yahoo.com>
2018-08-20 11:16:52 +0400

    Travis git branches updated, example tests deleted

--- in-iosUITests/in_iosUITests.swift
+++ in-iosUITests/in_iosUITests.swift
@@ -27,9 +27,4 @@ class in_iosUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 }
