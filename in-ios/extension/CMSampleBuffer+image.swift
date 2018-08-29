4ee967be03c3cd0ae552d5b1f27e013b5727a111
Rahman Mammadov <rehman_mv@yahoo.com>
2018-08-29 10:57:36 +0400

    CameraPreview fixes for test

---
+++ in-ios/extension/CMSampleBuffer+image.swift
@@ -0,0 +1,9 @@
+//
+//  CMSampleBuffer+image.swift
+//  in-ios
+//
+//  Created by Rahman Mammadov on 8/29/18.
+//  Copyright © 2018 com.innodemneurosciences. All rights reserved.
+//
+
+import Foundation
