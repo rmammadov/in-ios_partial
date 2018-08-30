8e600bd1ab44c14f8abc2846c43a2fcf9da7cb1f
Rahman Mammadov <rehman_mv@yahoo.com>
2018-08-30 17:16:08 +0400

    Image orientation fix

---
+++ in-ios/extension/UIImage+fixedOrientation.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+fixedOrientation.swift
+//  in-ios
+//
+//  Created by Rahman Mammadov on 8/30/18.
+//  Copyright © 2018 com.innodemneurosciences. All rights reserved.
+//
+
+import Foundation
