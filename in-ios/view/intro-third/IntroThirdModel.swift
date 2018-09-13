843773a8f55a154cf1510aa899b435f8294dba92
Rahman Mammadov <rehman_mv@yahoo.com>
2018-09-13 13:30:10 +0400

    IntroThird skeletion classes added

---
+++ in-ios/view/intro-third/IntroThirdModel.swift
@@ -0,0 +1,13 @@
+//
+//  IntroThirdModel.swift
+//  in-ios
+//
+//  Created by Rahman Mammadov on 9/13/18.
+//  Copyright © 2018 com.innodemneurosciences. All rights reserved.
+//
+
+import Foundation
+
+class IntroThirdModel: BaseModel {
+    
+}
---
+++ in-ios/view/intro-third/IntroThirdViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  IntroThirdViewModel.swift
+//  in-ios
+//
+//  Created by Rahman Mammadov on 9/13/18.
+//  Copyright © 2018 com.innodemneurosciences. All rights reserved.
+//
+
+import UIKit
+
+class IntroThirdViewModel: BaseViewModel {
+
+}
