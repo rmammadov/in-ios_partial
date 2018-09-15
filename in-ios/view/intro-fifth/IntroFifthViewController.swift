e0c57a1c436f4f69aa714720a1b4c819fbc7b960
Rahman Mammadov <rehman_mv@yahoo.com>
2018-09-15 12:22:52 +0400

    IntroFourth and IntroFifth skeleton

---
+++ in-ios/view/intro-fifth/IntroFifthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IntroFifthViewController.swift
+//  in-ios
+//
+//  Created by Rahman Mammadov on 9/15/18.
+//  Copyright © 2018 com.innodemneurosciences. All rights reserved.
+//
+
+import UIKit
+
+class IntroFifthViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
