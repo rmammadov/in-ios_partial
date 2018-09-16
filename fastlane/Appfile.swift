54423ebce8e73486c9a062b1fcf2ca401cc6c945
Yves Desgagne <yves@khome.org>
2018-09-16 19:59:52 -0400

    First test for travis and fastlane.

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.innodemneurosciences.in-ios" } // The bundle identifier of your app
+var appleID: String { return "dev@innodemneurosciences.com" } // Your Apple email address
+
+var itcTeam: String? { return "119138944" } // App Store Connect Team ID
+var teamID: String { return "954DRPZZ57" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
