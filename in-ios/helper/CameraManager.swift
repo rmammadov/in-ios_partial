ec9c0651756e68f1bb3d5da9cc98ca206f994e36
Rahman Mammadov <rehman_mv@yahoo.com>
2018-09-25 18:48:42 +0400

    Video resolution decreased to 320X240

--- in-ios/helper/CameraManager.swift
+++ in-ios/helper/CameraManager.swift
@@ -203,7 +203,7 @@ extension CameraManager: AVCaptureVideoDataOutputSampleBufferDelegate {
     func startSession() {
         DispatchQueue.global(qos: .background).async {
             if self.captureSession != nil {
-                self.captureSession?.sessionPreset = .vga640x480
+                self.captureSession?.sessionPreset = .low
                 self.captureSession?.startRunning()
 
                 self.deviceOrientation = UIDevice.current.orientation;
