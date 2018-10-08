688f3ea23cd2d8ea792b00321f79b9d9e22cc0ee
Rahman Mammadov <rehman_mv@yahoo.com>
2018-10-08 22:44:43 +0400

    Merge UiImage extensions

--- in-ios/extension/UIImageExtension.swift
+++ in-ios/extension/UIImageExtension.swift
@@ -1,8 +1,8 @@
 //
-//  UIImageExtension.swift
+//  UIImage+fixedOrientation.swift
 //  in-ios
 //
-//  Created by Piotr Soboń on 08/10/2018.
+//  Created by Rahman Mammadov on 8/30/18.
 //  Copyright © 2018 com.innodemneurosciences. All rights reserved.
 //
 
@@ -10,12 +10,32 @@ import UIKit
 
 extension UIImage {
     
-    /// UIImage from UIView
     convenience init(view: UIView) {
         UIGraphicsBeginImageContext(view.frame.size)
         view.layer.render(in: UIGraphicsGetCurrentContext()!)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         self.init(cgImage: (image?.cgImage)!)
+    }
+    
+    func rotate(radians: CGFloat) -> UIImage {
+        let rotatedSize = CGRect(origin: .zero, size: size)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(radians)))
+            .integral.size
+        UIGraphicsBeginImageContext(rotatedSize)
+        if let context = UIGraphicsGetCurrentContext() {
+            let origin = CGPoint(x: rotatedSize.width / 2.0,
+                                 y: rotatedSize.height / 2.0)
+            context.translateBy(x: origin.x, y: origin.y)
+            context.rotate(by: radians)
+            draw(in: CGRect(x: -origin.x, y: -origin.y,
+                            width: size.width, height: size.height))
+            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return rotatedImage ?? self
+        }
+        
+        return self
     }
 }
