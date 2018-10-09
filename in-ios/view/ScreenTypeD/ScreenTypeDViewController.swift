fac50de99165ff92d30236e62d586582cd94a313
Piotr Soboń <piotr.sobon@ready4s.com>
2018-10-09 17:05:18 +0200

    Add BodyPartRowView & BodyPartViewController

--- in-ios/view/ScreenTypeD/ScreenTypeDViewController.swift
+++ in-ios/view/ScreenTypeD/ScreenTypeDViewController.swift
@@ -9,26 +9,115 @@
 import UIKit
 import RxSwift
 
+private let nibMenuItem = "MenuItemCollectionViewCell"
+private let reuseIdentifier = "cellMenuItem"
+
 class ScreenTypeDViewController: BaseViewController {
     
-    let viewModel = ScreenTypeEViewModel()
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var containerView: UIView!
+    
+    private var isDisappear: Bool = true
+    let viewModel = ScreenTypeDViewModel()
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        onViewLoad()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        isDisappear = false
     }
-    */
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        isDisappear = true
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+            self.collectionView.collectionViewLayout.invalidateLayout()
+            self.collectionView.reloadData()
+        }
+    }
+    
+    private func replaceViewController() {
+        guard let nextVC = viewModel.getSelectedViewController() else { return }
+        if let childVC = children.first {
+            childVC.willMove(toParent: nil)
+            childVC.view.removeFromSuperview()
+            childVC.removeFromParent()
+        }
+        addChild(nextVC)
+        containerView.addSubview(nextVC.view)
+        nextVC.view.frame = containerView.bounds
+        nextVC.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        nextVC.didMove(toParent: self)
+    }
 
+}
+
+extension ScreenTypeDViewController {
+    private func onViewLoad() {
+        viewModel.loadItems()
+        setupCollectionView()
+        setSubscribers()
+    }
+    
+    private func setupCollectionView() {
+        let cellNib = UINib(nibName: nibMenuItem, bundle: nil)
+        collectionView.register(cellNib, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func setSubscribers() {
+        viewModel.getSelectedIndexPathObserver().subscribe(onNext: { [weak self] (indexPath) in
+            guard indexPath != nil else { return }
+            self?.replaceViewController()
+        }).disposed(by: disposeBag)
+    }
+}
+
+extension ScreenTypeDViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.getItems().count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                            for: indexPath) as? MenuItemCollectionViewCell,
+        let item = viewModel.getItem(for: indexPath)
+            else {
+                fatalError("Cannot dequeue cell with reuseIdentifier: \(reuseIdentifier)")
+        }
+        cell.setCell(url: item.icon?.url, label: item.translations?.first?.label)
+        return cell
+    }
+}
+
+extension ScreenTypeDViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        viewModel.setSelectedIndexPath(indexPath)
+    }
+}
+
+extension ScreenTypeDViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.size.width
+        var height: CGFloat = 0
+        if view.bounds.height / CGFloat(viewModel.getRowCount() - 1) > width + 7.0 + 7.0 + 17.0 {
+            height = width + 7.0 + 7.0 + 17.0
+        } else {
+            height = view.bounds.height / CGFloat(viewModel.getRowCount() - 1)
+        }
+        collectionViewHeight.constant = height * CGFloat(viewModel.getItems().count)
+        return CGSize(width: width, height: height)
+    }
 }
--- in-ios/view/ScreenTypeD/ScreenTypeDViewModel.swift
+++ in-ios/view/ScreenTypeD/ScreenTypeDViewModel.swift
@@ -14,4 +14,49 @@ class ScreenTypeDViewModel: BaseViewModel {
     weak var delegate: ScreenTypeCDelegate?
     var inputScreen: InputScreen!
     
+    private var items: [ButtonInputScreen] = []
+    private var viewControllers: [UIViewController] = []
+    private var selectedIndexPath = Variable<IndexPath?>(nil)
+    
+    func loadItems() {
+        guard let items = inputScreen.buttons, items.count > 0 else { return }
+        self.items = items
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        var vcs: [UIViewController] = []
+        items.forEach({ (button) in
+            if let vc = storyboard.instantiateViewController(withIdentifier: BodyPartViewController.identifier) as? BodyPartViewController {
+                vc.viewModel.button = button
+                vc.viewModel.delegate = delegate
+                vcs.append(vc)
+            }
+        })
+        selectedIndexPath.value = IndexPath(item: 0, section: 0)
+        self.viewControllers = vcs
+    }
+    
+    func getSelectedViewController() -> UIViewController? {
+        guard let index = getSelectedIndexPath()?.item, viewControllers.count > index else { return nil }
+        return viewControllers[index]
+    }
+    
+    func setSelectedIndexPath(_ indexPath: IndexPath?) {
+        selectedIndexPath.value = indexPath
+    }
+    
+    func getItems() -> [ButtonInputScreen] {
+        return items
+    }
+    
+    func getItem(for indexPath: IndexPath) -> ButtonInputScreen? {
+        guard indexPath.item < items.count else { return nil }
+        return items[indexPath.item]
+    }
+    
+    func getSelectedIndexPath() -> IndexPath? {
+        return selectedIndexPath.value
+    }
+    
+    func getSelectedIndexPathObserver() -> Observable<IndexPath?> {
+        return selectedIndexPath.asObservable()
+    }
 }
