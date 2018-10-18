bf023cc0a1bd018e1bd6144c489f7562a8ded3d5
Rahman Mammadov <rehman_mv@yahoo.com>
2018-10-18 17:59:55 +0400

    Fix of seguge not found for IntroFirstViewController

--- in-ios/view/intro-first/IntroFirstViewController.swift
+++ in-ios/view/intro-first/IntroFirstViewController.swift
@@ -27,7 +27,6 @@ class IntroFirstViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        performSegue(withIdentifier: SEGUE_IDENTIFIER_SHOW_HOME, sender: self)
         setUi()
     }
 
